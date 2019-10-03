ac18b042da7848c498c7588e8aef9279adc6acf7
Сергей <SergeyDolin@mail.ru>
2019-10-03 11:31:59 +0700

    Iteratrions

--- SpaceGeodesy.py
+++ SpaceGeodesy.py
@@ -38,6 +38,7 @@ groundItemApprox = GroundItem + GroundItemInitialError #приближенные
 
 
 #Определене единичных векторов топоцентрического направления на спутник
+#______________________________________________________________________
 u1 = (Sat1-groundItemApprox) / np.linalg.norm(Sat1-groundItemApprox)
 u2 = (Sat2-groundItemApprox) / np.linalg.norm(Sat2-groundItemApprox)
 u3 = (Sat3-groundItemApprox) / np.linalg.norm(Sat3-groundItemApprox)
@@ -54,11 +55,13 @@ f_ = np.array([f_1,f_2,f_3,f_4])
 
 X_new = np.dot(np.linalg.pinv(A), f_)
 print("X estimated", X_new)
-Xest = np.array([f_[0],f_[1],f_[2]])
+Xest = np.array([X_new[0],X_new[1],X_new[2]])
 
 delta = Xest - groundItemApprox
 groundItemApprox_1 = groundItemApprox + delta
-print(delta)
+print("First iteration", groundItemApprox_1)
+
+#_______________________________________________________________________
 u1_1 = (Sat1-groundItemApprox_1) / np.linalg.norm(Sat1-groundItemApprox_1)
 u2_2 = (Sat2-groundItemApprox_1) / np.linalg.norm(Sat2-groundItemApprox_1)
 u3_3 = (Sat3-groundItemApprox_1) / np.linalg.norm(Sat3-groundItemApprox_1)
@@ -71,15 +74,38 @@ f_22 =  -rho_measured[1] + np.dot(u2_2.transpose(), (Sat2))
 f_33 =  -rho_measured[2] + np.dot(u3_3.transpose(), (Sat3))
 f_44 =  -rho_measured[3] + np.dot(u4_4.transpose(), (Sat4))
 
-f_n = np.array([f_11,f_22,f_33,f_44])
+f_n1 = np.array([f_11,f_22,f_33,f_44])
 
-X_new1 = np.dot(np.linalg.pinv(A2), f_n)
-print(X_new1)
+X_new1 = np.dot(np.linalg.pinv(A2), f_n1)
+print("X estimated", X_new1)
 
-Xest1 = np.array([f_n[0],f_n[1],f_n[2]])
+Xest1 = np.array([X_new1[0],X_new1[1],X_new1[2]])
 delta1 = Xest1 - groundItemApprox_1
 groundItemApprox_2 = groundItemApprox_1 + delta1
-print(groundItemApprox_2)
+print("Second iteration", groundItemApprox_2)
+#________________________________________________________________________
+u1_ = (Sat1-groundItemApprox_2) / np.linalg.norm(Sat1-groundItemApprox_2)
+u2_ = (Sat2-groundItemApprox_2) / np.linalg.norm(Sat2-groundItemApprox_2)
+u3_ = (Sat3-groundItemApprox_2) / np.linalg.norm(Sat3-groundItemApprox_2)
+u4_ = (Sat4-groundItemApprox_2) / np.linalg.norm(Sat4-groundItemApprox_2)
+A1_ = np.vstack((u1_,u2_,u3_,u4_))
+A3 = np.hstack((A1_,E))
+
+f_111 =  -rho_measured[0] + np.dot(u1_.transpose(), (Sat1))
+f_222 =  -rho_measured[1] + np.dot(u2_.transpose(), (Sat2))
+f_333 =  -rho_measured[2] + np.dot(u3_.transpose(), (Sat3))
+f_444 =  -rho_measured[3] + np.dot(u4_.transpose(), (Sat4))
+
+f_n2 = np.array([f_111,f_222,f_333,f_444])
+
+X_new2 = np.dot(np.linalg.pinv(A3), f_n2)
+print("X estimated", X_new2)
+
+Xest2 = np.array([X_new2[0],X_new2[1],X_new2[2]])
+delta2 = Xest2 - groundItemApprox_2
+groundItemApprox_3 = groundItemApprox_2 + delta2
+print("Third iteration", groundItemApprox_3)
+#________________________________________________________________________
 
 
 #print(GroundItem - X_new1)
