e28c815a3fd4249405590911e0cf878a9c1e9f92
Сергей <SergeyDolin@mail.ru>
2019-10-08 10:26:15 +0700

    Разобраться с совместимостью NumPy и циклов

--- Iteration.py
+++ Iteration.py
@@ -4,24 +4,28 @@ from scipy import optimize
 from Model import*
 
 def Iterations(rho_measured, groundItemApprox):
-    #u1 = (Sat1-groundItemApprox) / np.linalg.norm(Sat1-groundItemApprox)
-    #u2 = (Sat2-groundItemApprox) / np.linalg.norm(Sat2-groundItemApprox)
-    #u3 = (Sat3-groundItemApprox) / np.linalg.norm(Sat3-groundItemApprox)
-    #u4 = (Sat4-groundItemApprox) / np.linalg.norm(Sat4-groundItemApprox)
-    #Orts(Satellites, groundItemApprox)
     orts = []
+    f = []
     for i in Satellites:
         orts.append((i-groundItemApprox) / np.linalg.norm(i-groundItemApprox))
-    #orts = [((Satellites.all()-groundItemApprox) / np.linalg.norm(Satellites.all()-groundItemApprox)) for i in Satellites]
+
+    #______________________________________________________________
+    # ТУТ БУДЕТ УЖАСНЫЙ КОСТЫЛЬ
+
     A1 = np.vstack((orts[0],orts[1],orts[2],orts[3]))
     A = np.hstack((A1,E))
+    
+    #______________________________________________________________
 
-    f_1 =  -rho_measured[0] + np.dot(orts[0].transpose(), (Sat1))
-    f_2 =  -rho_measured[1] + np.dot(orts[1].transpose(), (Sat2))
-    f_3 =  -rho_measured[2] + np.dot(orts[2].transpose(), (Sat3))
-    f_4 =  -rho_measured[3] + np.dot(orts[3].transpose(), (Sat4))
+    for j in range(0, Satellites.__len__()):
+        f.append(-rho_measured[j] + np.dot(orts[j].transpose(), (Satellites[j])))
+    
+    #______________________________________________________________
+    # И ТУТ ТОЖЕ ОН БУДЕТ    
 
-    f_ = np.array([f_1,f_2,f_3,f_4])
+    f_ = np.array([f[0],f[1],f[2],f[3]])
+
+    #______________________________________________________________
 
     X_new = np.dot(np.linalg.pinv(A), f_)
     print("X estimated", X_new)
@@ -29,9 +33,5 @@ def Iterations(rho_measured, groundItemApprox):
 
     delta = Xest - groundItemApprox
     groundItemApprox_1 = groundItemApprox + delta
-    print("First iteration", groundItemApprox_1)
+    #print("First iteration", groundItemApprox_1)
     return  groundItemApprox_1
-
-#def Orts(Satellites, groundItemApprox):
- #   orts = [((Satellites.any()-groundItemApprox) / np.linalg.norm(Satellites.any()-groundItemApprox)) for i in Satellites]
-  #  return orts
